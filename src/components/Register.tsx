13eaabf3d40f9e6f2bf8fcf4fa3bd6a764d0fbee
Zhang Junfan 장준범 <925209421@qq.com>
2026-02-02 15:55:10 +0900

    Register功能实现

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,10 +1,64 @@
-import React from 'react';
+"use client"
+import React, {ChangeEvent, FormEvent, useState} from 'react';
+import {Employee} from "@/redux/empSlice";
+import {useDispatch} from "react-redux";
+import {registerEmp} from "@/redux/empSlice";
+
+const initialState = {
+    id: "", name: "", age: 0, job: "", language: "", pay: 0
+}
 
 const Register = () => {
+    const [empInfo, setEmpInfo] = useState<Employee>(initialState);
+    const dispatch = useDispatch();
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setEmpInfo(prev => ({...prev, [name]: value}));
+    }
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatch(registerEmp(empInfo));
+        setEmpInfo(initialState);
+    }
     return (
-        <div>
-            Register
-        </div>
+        <form onSubmit={handleSubmit}>
+            <label>Name
+                <input
+                    type="text"
+                    name="name"
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Age
+                <input
+                    type="number"
+                    name="age"
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Job
+                <input
+                    type="text"
+                    name="job"
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Language
+                <input
+                    type="text"
+                    name="language"
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Pay
+                <input
+                    type="number"
+                    name="pay"
+                    onChange={handleChange}
+                />
+            </label>
+            <button type="submit">등록</button>
+        </form>
     );
 };
 
