c9e81261577c1d42e2f896778084e2a7bdb474eb
Zhang Junfan 장준범 <925209421@qq.com>
2026-02-02 16:09:20 +0900

    完善样式

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -3,6 +3,34 @@ import React, {ChangeEvent, FormEvent, useState} from 'react';
 import {Employee} from "@/redux/empSlice";
 import {useDispatch} from "react-redux";
 import {registerEmp} from "@/redux/empSlice";
+import styled from "styled-components";
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    margin: 20px auto;
+    padding: 20px;
+    border: 20px;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+`
+
+export const Label = styled.label`
+    margin-bottom: 10px;
+    display: flex;
+    flex-direction: column;
+    font-weight: bold;
+    color: #333;
+`
+
+export const Input = styled.input`
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    font-size: 14px;
+`
 
 const initialState = {
     id: "", name: "", age: 0, job: "", language: "", pay: 0
@@ -21,44 +49,44 @@ const Register = () => {
         setEmpInfo(initialState);
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Name
-                <input
+        <Form onSubmit={handleSubmit}>
+            <Label>Name
+                <Input
                     type="text"
                     name="name"
                     onChange={handleChange}
                 />
-            </label>
-            <label>Age
-                <input
+            </Label>
+            <Label>Age
+                <Input
                     type="number"
                     name="age"
                     onChange={handleChange}
                 />
-            </label>
-            <label>Job
-                <input
+            </Label>
+            <Label>Job
+                <Input
                     type="text"
                     name="job"
                     onChange={handleChange}
                 />
-            </label>
-            <label>Language
-                <input
+            </Label>
+            <Label>Language
+                <Input
                     type="text"
                     name="language"
                     onChange={handleChange}
                 />
-            </label>
-            <label>Pay
-                <input
+            </Label>
+            <Label>Pay
+                <Input
                     type="number"
                     name="pay"
                     onChange={handleChange}
                 />
-            </label>
+            </Label>
             <button type="submit">등록</button>
-        </form>
+        </Form>
     );
 };
 
